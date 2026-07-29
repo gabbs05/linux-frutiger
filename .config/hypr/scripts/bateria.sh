4cc0cca939dca79e91e6e82e34e7dc40be9588f2
gabbs05 <gabrielepradach@gmail.com>
2026-07-28 22:33:09 -0400

    Arreglo de efectos de sonidos

--- .config/hypr/scripts/bateria.sh
+++ .config/hypr/scripts/bateria.sh
@@ -4,8 +4,8 @@ ALERTA_ENVIADA=0
 while true; do
     # Lee el porcentaje y el estado (cargando/descargando) de la batería principal (BAT0)
     # Nota: Si tu batería se llama BAT1, cambia el BAT0 de las siguientes líneas.
-    BATERIA=$(cat /sys/class/power_supply/BAT0/capacity)
-    ESTADO=$(cat /sys/class/power_supply/BAT0/status)
+    BATERIA=$(cat /sys/class/power_supply/BAT1/capacity)
+    ESTADO=$(cat /sys/class/power_supply/BAT1/status)
 
     if [[ "$BATERIA" -le 20 && "$ESTADO" == "Discharging" ]]; then
         if [[ "$ALERTA_ENVIADA" == 0 ]]; then
